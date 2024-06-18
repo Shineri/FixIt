57af0c5fcc40fcff48cdd9434669f3cf86e7b311
Kiran <kiran2022003kumari@gmail.com>
2024-06-18 18:24:28 +0530

    resetpassword routers added/testing not done

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,7 +1,8 @@
 const express =require("express");
 const router = express.Router();
-const {signup,login,getProfile} = require ("../controllers/authController")
-const authenticate = require("../middlewares/authMiddleware")
+const {signup,login,getProfile} = require ("../controllers/authController");
+const authenticate = require("../middlewares/authMiddleware");
+const { requestPasswordReset, resetPassword } = require('../controllers/resetpassword');
 
 // Public routes
 router.post("/signup",signup);
@@ -11,5 +12,11 @@ router.post("/login",login);
 // Protected route - Get User Profile
 router.get('/profile', authenticate, getProfile);
    
+
+// Route to request password reset
+router.post('/request-reset-password', requestPasswordReset);
+
+// Route to handle actual password reset
+router.post('/reset-password/:token', resetPassword);
 
 module.exports = router;
