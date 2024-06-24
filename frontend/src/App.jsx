f17369054403b69772be3e3f2b357868f1caf2b3
Vani Tripathi <vanitripathi1103@gmail.com>
2024-06-24 22:52:54 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CreateAccountPage from "./pages/Authentication/CreateAccountPage";
 import Login from "./pages/Authentication/Login";
 import ComplainForm from "./pages/ComplainForm";
-import PaymentForm from "./pages/PaymentForm";
+
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import ManagerProfile from './pages/ManagerProfile';
@@ -22,7 +22,7 @@ const App = () => {
           <Route path="/CreateAccountPage" element={<CreateAccountPage/>} />
           <Route path="/Login" element={<Login/>} />
           <Route path="/ComplainForm" element={<ComplainForm/>} />
-          <Route path="/PaymentForm" element={<PaymentForm/>} />
+        
           <Route path ="/UserProfile" element={<UserProfile/>}/>
           <Route path ="/ManagerProfile" element={<ManagerProfile/>}/>
           {/* Add other routes as needed */}
