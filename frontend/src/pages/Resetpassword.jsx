0a8b6c0ecfa8d8da6553744864a434fc9f31a754
RIYA <116687118+riya1901@users.noreply.github.com>
2024-07-16 23:52:36 +0530

    resetpasswordpage updated

--- frontend/src/pages/Resetpassword.jsx
+++ frontend/src/pages/Resetpassword.jsx
@@ -19,7 +19,7 @@ const Resetpassword = () => {
     }
 
     try {
-      const response = await axios.post(`http://localhost:3000/reset-password/${resetToken}`, {
+      const response = await axios.post(`http://localhost:3000/api/v1/auth/reset-password/:token`, {
         newPassword,
         confirmPassword,
       });
